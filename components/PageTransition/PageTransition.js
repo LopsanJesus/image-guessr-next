62e562555a027b20bbe20efeec17372f38c599b5
Jesus L <lopsanjesus@gmail.com>
2026-03-12 00:50:43 +0100

    fix error in levels page

--- components/PageTransition/PageTransition.js
+++ components/PageTransition/PageTransition.js
@@ -1,11 +1,20 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const PageTransition = ({ children }) => {
   const pathname = usePathname();
+  const [animating, setAnimating] = useState(false);
+
+  useEffect(() => {
+    setAnimating(false);
+    const id = requestAnimationFrame(() => setAnimating(true));
+    return () => cancelAnimationFrame(id);
+  }, [pathname]);
+
   return (
-    <div key={pathname} className="animate-page-enter">
+    <div className={animating ? "animate-page-enter" : ""}>
       {children}
     </div>
   );
